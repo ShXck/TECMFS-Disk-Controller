3dd57d3417ff6a0d879783b1b35ab04791d469b3
Marcelo <mss97@hotmail.es>
2017-11-17 00:08:28 -0600

    Cambio en la recepción de datos

--- raid_h/DiskNode.cpp
+++ raid_h/DiskNode.cpp
@@ -31,27 +31,29 @@ void Disk_Node::execute( network::Processed_Tmp tmp, sf::TcpSocket* socket ) {
 				chunk_container.insert( chunk_container.end(), tmp_cont.begin(), tmp_cont.end() );
 			}
 
-			std::sort( chunk_container.begin(), chunk_container.end() );
+			if( !chunk_container.empty() ) {
+				std::sort( chunk_container.begin(), chunk_container.end() );
 
-			for( auto& vc : chunk_container ) {
-				/*std::string og_bytes;
-				std::vector<std::string> sub_bins;
-				std::string curr_bin = vc.video_binary_data;
-				for( int i = 0; i < curr_bin.length() / 8; i++ ) {
-					sub_bins.push_back( curr_bin.substr( 0, 8 ) );
-					curr_bin.erase( 0, 8 );
+				for( auto& vc : chunk_container ) {
+					/*std::string og_bytes;
+					std::vector<std::string> sub_bins;
+					std::string curr_bin = vc.video_binary_data;
+					for( int i = 0; i < curr_bin.length() / 8; i++ ) {
+						sub_bins.push_back( curr_bin.substr( 0, 8 ) );
+						curr_bin.erase( 0, 8 );
+					}
+					for( unsigned int j = 0; j < sub_bins.size(); j++ ) {
+						og_bytes += bit_to_byte( sub_bins[j] );
+					}*/
+
+					std::string ret_msg = JHandler::build_retrv_msg( tmp.video_id, vc.chunk_order, vc.mat_number , (int)Instruction::RETRV_INSTR );
+					sf::Packet _packet;
+					_packet << vc.tmp_bytes << ret_msg;
+					socket->send( _packet );
 				}
-				for( unsigned int j = 0; j < sub_bins.size(); j++ ) {
-					og_bytes += bit_to_byte( sub_bins[j] );
-				}*/
-
-				std::string ret_msg = JHandler::build_retrv_msg( tmp.video_id, vc.chunk_order, vc.mat_number , (int)Instruction::RETRV_INSTR );
-				sf::Packet _packet;
-				_packet << vc.tmp_bytes << ret_msg;
-				socket->send( _packet );
+				send_ready_msg( socket );  //TODO: ENVIAR HASTA QUE SE ENVIEN TODOS LOS MATS
+				std::this_thread::sleep_for( std::chrono::milliseconds( 250 ) );
 			}
-			send_ready_msg( socket );  //TODO: ENVIAR HASTA QUE SE ENVIEN TODOS LOS MATS
-			std::this_thread::sleep_for( std::chrono::milliseconds( 250 ) );
 		}
 	}
 }
